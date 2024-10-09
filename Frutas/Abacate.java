b27782c9719f087ceff6507c0693b63bb910ac72
LuanAlmeidaValenca <145447231+LuanAlmeidaValenca@users.noreply.github.com>
2024-10-09 19:50:14 -0300

    Update Abacate.java

--- Frutas/Abacate.java
+++ Frutas/Abacate.java
@@ -1,6 +1,23 @@
 package Frutas;
 
+/**
+ * A classe Abacate representa uma fruta específica que possui um poder associado
+ * É uma subclasse de FrutaComPoder e herda suas propriedades, como nome, posição, e se está bichada
+ * Além disso, define o poder de valor 2
+ */
+
 public class Abacate extends FrutaComPoder {
+	
+	/**
+     * Construtor da classe Abacate
+     * Inicializa um objeto Abacate com o nome, posição, e estado (bichada ou não)
+     * Define o poder do abacate para 2
+     * 
+     * @param nome O nome da fruta
+     * @param posicao A posição da fruta na floresta, representada por um array de inteiros [X, Y]
+     * @param bichada Um booleano que indica se a fruta está bichada (true) ou não (false)
+     */
+	
     public Abacate(String nome, int[] posicao, boolean bichada) {
         super(nome, posicao, bichada);
         super.setPoder(2);
