f0d1adec3c9faf4f0fbc0bb9eacb075cd4ca8deb
LuanAlmeidaValenca <145447231+LuanAlmeidaValenca@users.noreply.github.com>
2024-10-09 19:55:02 -0300

    javadoc Coco.java

--- Frutas/Coco.java
+++ Frutas/Coco.java
@@ -1,6 +1,23 @@
 package Frutas;
 
+/**
+ * A classe Coco representa uma fruta específica que possui um poder associado
+ * É uma subclasse de FrutaComPoder e herda suas propriedades, como nome, posição, e se está bichada
+ * Além disso, define o poder de valor 2
+ */
+
 public class Coco extends FrutaComPoder {
+	
+	/**
+     * Construtor da classe Coco.
+     * Inicializa um objeto Coco com o nome, posição, e estado (bichada ou não)
+     * Define o poder do abacate para 1
+     * 
+     * @param nome O nome da fruta
+     * @param posicao A posição da fruta na floresta, representada por um array de inteiros [X, Y]
+     * @param bichada Um booleano que indica se a fruta está bichada (true) ou não (false)
+     */
+	
     public Coco(String nome, int[] posicao, boolean bichada) {
         super(nome, posicao, bichada);
         super.setPoder(1);
