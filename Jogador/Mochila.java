ebf8b49809927ad7e4c5f6026b9b2bcec4876295
LuanAlmeidaValenca <145447231+LuanAlmeidaValenca@users.noreply.github.com>
2024-10-09 19:57:59 -0300

    javadoc Mochila.java

--- Jogador/Mochila.java
+++ Jogador/Mochila.java
@@ -2,23 +2,46 @@ package Jogador;
 
 import Frutas.Fruta;
 
+/**
+ * A classe Mochila representa a mochila de um jogador, onde ele pode armazenar frutas
+ */
 public class Mochila {
     private Fruta[] frutas;
     private int capacidade;
 
+    /**
+     * Construtor da classe Mochila
+     * Inicializa uma nova mochila com capacidade para 10 frutas
+     */
     public Mochila(){
         this.frutas = new Fruta[10];
         this.capacidade = 10;
     }
-
+    
+    /**
+     * Obtém a capacidade atual da mochila
+     *
+     * @return A capacidade da mochila
+     */
     public int getCapacidade(){
         return this.capacidade;
     }
 
+    /**
+     * Obtém as frutas armazenadas na mochila
+     *
+     * @return Um array de frutas na mochila
+     */
     public Fruta[] getFrutas(){
         return this.frutas;
     }
 
+    /**
+     * Adiciona uma fruta à mochila, se houver espaço disponível
+     *
+     * @param fruta A fruta a ser adicionada
+     * @return true se a fruta foi adicionada com sucesso, false se a mochila está cheia
+     */
     public boolean adicionarFruta(Fruta fruta){
         for (int i = 0; i < this.frutas.length; i++) {
             if (this.frutas[i] == null) {
@@ -30,6 +53,12 @@ public class Mochila {
         return false; 
     }
 
+    /**
+     * Remove uma fruta da mochila
+     *
+     * @param fruta A fruta a ser removida
+     * @return true se a fruta foi removida com sucesso, false se a fruta não estava na mochila
+     */
     public boolean removerFruta(Fruta fruta){
         for(int i = 0; i < this.frutas.length; i++){
             if(this.frutas[i] == fruta){
